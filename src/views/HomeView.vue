5deb4b19c6ef9e2c4c6ff2910e5cd4b55d7d1e66
Enokek <enok.ekelund@gmail.com>
2023-02-07 12:05:14 +0100

    Added dropdown menu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import VideoButton from "../components/VideoButton.vue";
+import DropDownMenu from "../components/DropDownMenu.vue";
 </script>
 
 <template>
@@ -7,6 +8,7 @@ import VideoButton from "../components/VideoButton.vue";
     <h1 class="text-2xl text-white">VideoApp</h1>
 
     <div class="rounded-xl bg-white p-2">
+      <DropDownMenu />
       <video class="" src=""></video>
       <VideoButton buttonText="Knapp" />
     </div>
