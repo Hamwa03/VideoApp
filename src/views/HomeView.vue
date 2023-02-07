333cd525b6cc2872d25e2a74fd2285c9b4def836
Enokek <enok.ekelund@gmail.com>
2023-02-07 12:36:20 +0100

    Small change, HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,9 +5,10 @@ import DropDownMenu from "../components/DropDownMenu.vue";
 
 <template>
   <main class="text-center">
-    <h1 class="text-2xl text-white">VideoApp</h1>
+    <h1 class="text-5xl text-white">VideoApp</h1>
     <DropDownMenu />
-    <div class="rounded-xl bg-white flex justify-center p-2">
+
+    <div class="rounded-xl bg-white flex justify-center p-2 mx-8 mt-4">
       <video class="w-3/4" src=""></video>
     </div>
   </main>
