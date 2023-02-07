7a26f8298ec806e12f43e9444e5002d2a8534164
williampettersson <william.c.pettersson@gmail.com>
2023-02-07 13:26:50 +0100

    fixed buttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,14 @@ import DropDownMenu from "../components/DropDownMenu.vue";
 </script>
 
 <template>
-  <main class="text-center">
+  <main class="text-center bg-slate-900">
     <h1 class="text-5xl text-white mt-2">VideoApp</h1>
-    <DropDownMenu />
-    <VideoButton buttonText="Ny video" />
+    <div class="p-4 flex justify-center">
+      <DropDownMenu />
+      <VideoButton buttonText="Ny video" />
+    </div>
 
-    <div class="rounded-xl bg-white flex justify-center p-2 mx-8 mt-4">
+    <div class="rounded-xl bg-white flex justify-center p-2 mx-8">
       <video class="w-3/4" src=""></video>
     </div>
   </main>
